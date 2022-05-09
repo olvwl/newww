4547a90833b579d2b12615ec552e56822782c93b
Melania Syafrida P <54350703+olvwl@users.noreply.github.com>
2022-05-09 16:22:29 +0700

    fix identasi

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, File
+from segmentation import get_yolov5, get_image_from_bytes
+from starlette.responses import Response
+import io
+from PIL import Image
+import json
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+model = get_yolov5()
+
+@app.post("/object-to-json")
+async def detect_food_return_json_result(file: bytes = File(...)):
+    input_image = get_image_from_bytes(file)
+    results = model(input_image)
+    detect_res = results.pandas().xyxy[0].to_json(orient="records")  # JSON img1 predictions
+    detect_res = json.loads(detect_res)
+    return {"result": detect_res}
+
+
+@app.post("/object-to-img")
+async def detect_food_return_base64_img(file: bytes = File(...)):
+    input_image = get_image_from_bytes(file)
+    results = model(input_image)
+    results.render()  # updates results.imgs with boxes and labels
+    for img in results.imgs:
+        bytes_io = io.BytesIO()
+        img_base64 = Image.fromarray(img)
+        img_base64.save(bytes_io, format="jpeg")
+    return Response(content=bytes_io.getvalue(), media_type="image/jpeg")
+
+if __name__ == '__main__':
+
+    uvicorn.run(app, host='0.0.0.0', port=8000, debug=True)
