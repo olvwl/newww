dc23747715437e94e5b4da01ae0abaaad7c25b9e
Melania Syafrida P <54350703+olvwl@users.noreply.github.com>
2022-05-13 19:04:04 +0700

    update save image to directory

--- main.py
+++ main.py
@@ -20,16 +20,22 @@ async def detect_food_return_json_result(file: bytes = File(...)):
 
 
 @app.post("/object-to-img")
-async def detect_food_return_base64_img(file: bytes = File(...)):
+async def detect_return_base64_img(file: bytes = File(...)):
     input_image = get_image_from_bytes(file)
     results = model(input_image)
     results.render()  # updates results.imgs with boxes and labels
+    ##print("kucing" ,results)
     for img in results.imgs:
         bytes_io = io.BytesIO()
         img_base64 = Image.fromarray(img)
         img_base64.save(bytes_io, format="jpeg")
-    return Response(content=bytes_io.getvalue(), media_type="image/jpeg")
+        ##print("kucing", bytes_io)
+        imageResult = Response(bytes_io.getvalue(), media_type= "image/jpeg")
+    
+        image_data = bytes_io.getvalue()
 
-if __name__ == '__main__':
+        with open('$image.jpeg','wb') as image:
+            image.write(image_data)
+            image.close()
 
-    uvicorn.run(app, host='0.0.0.0', port=8000, debug=True)
+    return imageResult
