8948cd3593e4cffde5933d79b9be9047b4fcfa6e
Melania Syafrida P <54350703+olvwl@users.noreply.github.com>
2022-05-13 20:37:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,54 @@ import os
 from fastapi import FastAPI, File
 from segmentation import get_yolov5, get_image_from_bytes
 from starlette.responses import Response
+from starlette.responses import JSONResponse
 import io
 from PIL import Image
 import json
+import numpy as np
+from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
+from pydantic import BaseModel
 
-app = FastAPI()
 
 model = get_yolov5()
 
-path = "home/syafridamelania/github/newww"
+app = FastAPI(
+    title="Custom YOLOV5 Machine Learning API",
+    description="""Obtain object value out of image
+                    and return image and json result""",
+    version="0.0.1",
+)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+path = "D:/flask/fastAPI/yolov5-fastapi"
+
+class json_object(BaseModel):
+    imageUrl : str
+    
 
 @app.post("/object-to-json")
-async def detect_food_return_json_result(file: bytes = File(...)):
+async def detect_return_json_result(file: bytes = File(...)):
     input_image = get_image_from_bytes(file)
     results = model(input_image)
     detect_res = results.pandas().xyxy[0].to_json(orient="records")  # JSON img1 predictions
     detect_res = json.loads(detect_res)
+    print(detect_res)
     return {"result": detect_res}
-
 
 @app.post("/object-to-img")
 async def detect_return_base64_img(file: bytes = File(...)):
@@ -33,6 +61,7 @@ async def detect_return_base64_img(file: bytes = File(...)):
         bytes_io = io.BytesIO()
         img_base64 = Image.fromarray(img)
         img_base64.save(bytes_io, format="jpeg")
+        ##print("kucing", bytes_io)
         imageResult = Response(bytes_io.getvalue(), media_type= "image/jpeg")
     
         image_data = bytes_io.getvalue()
@@ -43,4 +72,4 @@ async def detect_return_base64_img(file: bytes = File(...)):
 
         filePath = os.path.join(path, "image.jpeg")
 
-    return FileResponse(FilePath)
+    return FileResponse(filePath)
